bce6e6f594e8f4710ecaf55d281e61ec6e428d18
Kseniya <znakioma@mail.ru>
2016-04-24 14:11:00 +0300

    SnakeLesson6

---
+++ Snake/Point.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class Point
+    {
+        public int x;
+        public int y;
+        public char sym;
+
+        public Point(int _x, int _y, char _sym ) // Конструктор  для инкапсуляции (должен иметь имя класса) 
+        {
+            x = _x; // переменным перисваются новые значения в конструкторе 
+            y = _y;
+            sym = _sym;
+
+           
+
+        }
+
+        public Point(Point p)
+        {
+            x = p.x;
+            y = p.y;
+            sym = p.sym;
+        }
+        public void Move ( int offset, Direction direction)
+        {
+            if (direction == Direction.right)
+                x = x + offset;
+
+            if (direction == Direction.Left)
+                x = x - offset;
+
+        }
+
+
+        public void Draw()
+        {
+            Console.SetCursorPosition(x, y); // забирает значения переменных, преобразовывает в координаты  
+            Console.Write(sym); // выводит символ по заданным координатам 
+        }
+    }
+}
---
+++ Snake/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // установка размера консоли 
+
+            Console.SetBufferSize(80,25);
+
+            // отрисовка рамочки
+            
+            HorizontalLine upline = new HorizontalLine(0,78,0,'+');
+            upline.Drown();
+
+            HorizontalLine downline = new HorizontalLine(0, 78, 24, '+');
+            downline.Drown();
+
+            VerticalLine leftline = new VerticalLine(0, 24, 0, '+');
+            leftline.Drown();
+
+            VerticalLine rightline = new VerticalLine(0, 24, 78, '+');
+            rightline.Drown();
+
+            // отрисовка точки
+
+            Point p = new Point(4, 5, '*');
+            Snake snake = new Snake(p, 4, Direction.right);
+            snake.Drown();
+
+          
+
+            Console.ReadKey();
+
+            
+        }
+        
+        
+    }
+}
